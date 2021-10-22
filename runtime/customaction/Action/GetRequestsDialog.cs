8f868a86e87caa7124105ebe43f73adc5ac7afa4
shujaatsiddiqui <shujaatsiddiqui>
2021-10-21 18:54:42 -0600

    Revert "attempt 02"
    This reverts commit 4e3645de40bcc4a15f00c48d5208203a602b9771.

--- runtime/customaction/Action/GetRequestsDialog.cs
+++ runtime/customaction/Action/GetRequestsDialog.cs
@@ -1,14 +1,12 @@
 ﻿using AdaptiveExpressions.Properties;
 using Microsoft.Bot.Builder.Dialogs;
 using Microsoft.Bot.Schema;
-using Microsoft.Bot.Schema.Teams;
 using Microsoft.BotFramework.Composer.DAL.Implementation;
 using Microsoft.BotFramework.Composer.DAL.Services.Abstraction;
 using Microsoft.BotFramework.Composer.Intermediator;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Converters;
 using System;
 using System.Collections.Generic;
 using System.Runtime.CompilerServices;
@@ -55,67 +53,27 @@ namespace Microsoft.BotFramework.Composer.CustomAction.Action
         public override async Task<DialogTurnResult> BeginDialogAsync(DialogContext dc, object options = null, CancellationToken cancellationToken = default)
         {
             Activity replyActivity = null;
-            try
+            var activity = dc.Context.Activity;
+
+            IList<ConnectionRequest> connectionRequests =
+                        _messageRouter.RoutingDataManager.GetConnectionRequests();
+
+            replyActivity = activity.CreateReply();
+
+            if (connectionRequests.Count == 0)
             {
-                var activity = dc.Context.Activity;
-
-                IList<ConnectionRequest> connectionRequests =
-                            _messageRouter.RoutingDataManager.GetConnectionRequests();
-
-                replyActivity = activity.CreateReply();
-
-                if (connectionRequests.Count == 0)
-                {
-                    replyActivity.Text = "No pending requests";
-                }
-                else
-                {
-                    replyActivity.Attachments = CommandCardFactory.CreateMultipleConnectionRequestCards(
-                        connectionRequests, userService, activity.Recipient?.Name);
-                }
-                List<ConversationReference> cr = new List<ConversationReference>();
-                foreach (var item in connectionRequests)
-                {
-                    item.Requestor.User.Id = null;
-                    item.Requestor.Conversation.Id = null;
-                    item.Requestor.ServiceUrl = null;
-                    item.ConnectionRequestTime = DateTime.Now;
-                }
-                //replyActivity.ChannelData = dc.Context.Activity.GetChannelData<TeamsChannelData>();
-                replyActivity.ChannelData = JsonConvert.SerializeObject(connectionRequests, new NoColonIsoDateTimeConverter());
-
-                replyActivity.Text = "CD | " + replyActivity.ChannelData;
-            }
-            catch (Exception ex)
-            {
-                replyActivity.Text = ex.Message + " | " + ex.StackTrace;
-            }
-
-            await dc.Context.SendActivityAsync(replyActivity, cancellationToken);
-            return await dc.EndDialogAsync(null, cancellationToken);
-        }
-    }
-
-    public class NoColonIsoDateTimeConverter : IsoDateTimeConverter
-    {
-        public NoColonIsoDateTimeConverter()
-        {
-            DateTimeFormat = "ABYYYY";
-        }
-
-        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
-        {
-            if (value is DateTime)
-            {
-                var dateTime = (DateTime)value;
-                var text = dateTime.ToString(DateTimeFormat);
-                text = text.Remove(text.Length - 3, 1);
-                writer.WriteValue(text);
+                replyActivity.Text = "No pending requests";
             }
             else
             {
-                throw new JsonSerializationException("Unexpected value when converting date. Expected DateTime");
+                replyActivity.Attachments = CommandCardFactory.CreateMultipleConnectionRequestCards(
+                    connectionRequests, userService, activity.Recipient?.Name);
             }
+
+            replyActivity.ChannelData = JsonConvert.SerializeObject(connectionRequests);
+            await dc.Context.SendActivityAsync(replyActivity, cancellationToken);
+
+            return await dc.EndDialogAsync(null, cancellationToken);
         }
     }
 }
